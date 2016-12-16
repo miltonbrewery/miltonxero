c9075b49c9e1ef099724c50bf62b15f3e169767a
Stephen Early <steve@assorted.org.uk>
2016-12-16 13:38:10 +0000

    Revert "Change rounding rules for multiple items"
    This reverts commit b24a22f44b4edcd8c6b7ac5254a5269e53680d74.
    
    I had forgotten that the problem here was in Xero itself and not in the
    invoicing tool.

--- invoicer/views.py
+++ invoicer/views.py
@@ -373,9 +373,8 @@ class InvoiceItemBand:
     def __init__(self, item, priceband):
         self.priceperbarrel, self.account, self.reasons \
             = priceband.apply_rules_for(item)
-        self.price_per_unit = (self.priceperbarrel * item.unit.size)\
+        self.price = (self.priceperbarrel * item.barrels)\
             .quantize(Decimal("0.01"), rounding=ROUND_HALF_UP)
-        self.price = self.price_per_unit * item.items
         self.priceincvat = (self.price * settings.VAT_MULTIPLIER)\
             .quantize(Decimal("0.01"), rounding=ROUND_HALF_UP)
         
