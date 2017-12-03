f91e087dbe8d50e86e52582896303bb67773e510
Stephen Early <steve@assorted.org.uk>
2017-12-03 16:06:11 +0000

    Use callables in urls instead of python paths
    Use of python paths in URLs is deprecated after django-1.8

--- invoicer/urls.py
+++ invoicer/urls.py
@@ -1,17 +1,17 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
+from . import views
 
-urlpatterns = patterns(
-    'invoicer.views',
-    url(r'^priceband/(?P<bandid>\d+)/$', 'priceband'),
-    url(r'^invoice/$', 'startinvoice'),
-    url(r'^invoice/(?P<contactid>[a-f0-9-]+)/$', 'invoice',
+urlpatterns = [
+    url(r'^priceband/(?P<bandid>\d+)/$', views.priceband),
+    url(r'^invoice/$', views.startinvoice),
+    url(r'^invoice/(?P<contactid>[a-f0-9-]+)/$', views.invoice,
         name="invoice"),
-    url(r'^bill/(?P<contactid>[a-f0-9-]+)/$', 'invoice', {'bill': True},
+    url(r'^bill/(?P<contactid>[a-f0-9-]+)/$', views.invoice, {'bill': True},
         name="bill"),
-    url(r'^product/$', 'product'),
-    url(r'^product/(?P<productid>\d+)/$', 'product'),
-    url(r'^ajax/contact-completions.json$', 'contact_completions'),
-    url(r'^ajax/item-completions.json$', 'item_completions'),
-    url(r'^ajax/item-details.json$', 'item_details'),
-    url(r'^ajax/productcode-check.json$', 'productcode_check'),
-)
+    url(r'^product/$', views.product),
+    url(r'^product/(?P<productid>\d+)/$', views.product),
+    url(r'^ajax/contact-completions.json$', views.contact_completions),
+    url(r'^ajax/item-completions.json$', views.item_completions),
+    url(r'^ajax/item-details.json$', views.item_details),
+    url(r'^ajax/productcode-check.json$', views.productcode_check),
+]
--- miltonxero/urls.py
+++ miltonxero/urls.py
@@ -1,13 +1,10 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 admin.site.site_header = 'Milton Invoice Tool'
+import invoicer.views, invoicer.urls
 
-urlpatterns = patterns(
-    '',
-    # Examples:
-    # url(r'^$', 'miltonxero.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'invoicer.views.startinvoice'),
-    url(r'^', include('invoicer.urls')),
-)
+    url(r'^$', invoicer.views.startinvoice),
+    url(r'^', include(invoicer.urls.urlpatterns)),
+]
