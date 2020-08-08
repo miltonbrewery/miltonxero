10d9d043f592f042a7b8c7013e86160952a315ac
Stephen Early <steve@assorted.org.uk>
2020-08-08 12:19:07 +0100

    Update production settings

--- miltonxero/settings_production.py
+++ miltonxero/settings_production.py
@@ -9,3 +9,22 @@ MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media")
 MEDIA_URL = '/media/'
 
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': '/home/miltonxero/live/django.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    },
+}
